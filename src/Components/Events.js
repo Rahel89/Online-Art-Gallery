745a5aace9fc55b381883d4992b54b5e77fc40d4
Rahel89 <rahelayana123@gmail.com>
2025-02-10 01:21:45 +0300

    updated the form and added pictures

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import  { useState } from "react";
+import event1 from '../images/event1.jpg'
+import event2 from '../images/event7.jpg'
+import event6 from '../images/event6.jpg'
+import event4 from '../images/event4.jpg'
+
+const images = [
+  require("../images/event1.jpg"),
+  require("../images/event7.jpg"),
+  require("../images/event6.jpg"),
+  require("../images/event4.jpg"),
+];
 
 const events = [
     "Art Exhibition",
@@ -7,22 +19,34 @@ const events = [
     "Sculpture Showcase",
     "Painting Masterclass"
   ];
-  
   const Events = () => {
+    const [index, setIndex] = useState(0);
+
+  const handleNextImage = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
     return (
       <div className="container text-center mt-5">
         <div className="row">
           <div className="col-md-6">
             <h2>Upcoming Events</h2>
-            <img src= "" alt="Upcoming Event" className="img-fluid mt-3" />
+            <img src= {images[index]} alt="Upcoming Event" className="img-fluid mt-3" />
+            <button className="btn btn-secondary mt-3" onClick={handleNextImage}>
+            Next
+          </button>
           </div>
           
           <div className="col-md-6">
             <h2>Register for an Event</h2>
             <form className="mt-3">
             <div className="mb-3 d-flex align-items-center">
-              <label className="form-label me-3" style={{ minWidth: "120px" }}>Name</label>
-              <input type="text" className="form-control" placeholder="Enter your name" />
+              <label className="form-label me-3" style={{ minWidth: "120px" }}>First Name</label>
+              <input type="text" className="form-control" placeholder="Enter your first name" />
+            </div>
+            <div className="mb-3 d-flex align-items-center">
+              <label className="form-label me-3" style={{ minWidth: "120px" }}>Last Name</label>
+              <input type="text" className="form-control" placeholder="Enter your last name" />
             </div>
             <div className="mb-3 d-flex align-items-center">
               <label className="form-label me-3" style={{ minWidth: "120px" }}>Email</label>
