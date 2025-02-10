c3bfc10f8b5899c592ca9c26aab6e62e2a9bc1b8
Rahel89 <rahelayana123@gmail.com>
2025-02-10 23:09:22 +0300

    added categories to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,28 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
+const categories = [
+    {
+      title: "Painting",
+      image: require(""),
+      link: "/gallery",
+    },
+    {
+      title: "Photography",
+      image: require(""),
+      link: "/photography",
+    },
+    {
+      title: "Sculpture",
+      image: require(""),
+      link: "/sculpture",
+    }
+  ];
+
 const Home = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
-                <a className="navbar-brand" href="#">Logo</a>
                 <div className="navbar-nav">
                     <Link to="/gallery" className="nav-link mx-3">Main Gallery</Link>
                     <a className="nav-link mx-3" href="#">Submit Artwork</a>
@@ -19,18 +36,40 @@ const Home = () => {
                 <div className="row">
                     <div className="col">
                         <h1>Welcome to Our Art Gallery</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>Presenting up and coming and renowned Ethiopian creatives </p>
                     </div>
-                    <div className="col">
-                        <img src="image.jpg" className="img-fluid" alt="Image" />
-                    </div>
+                  
                 </div>
             </div>
 
+            <div className="container my-5">
+      <h2 className="text-center mb-4">Explore Our Categories</h2>
+      <div className="row">
+        {categories.map((category, index) => (
+          <div key={index} className="col-md-4">
+            <div className="card shadow-lg">
+              <img src={category.image} className="card-img-top" alt={category.title} />
+              <div className="card-body text-center">
+                <h5 className="card-title">{category.title}</h5>
+                <a href={category.link} className="btn btn-primary">Explore {category.title}</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+
+
             <div className="container bg-light py-4 my-4">
                 <h2>About Us</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <h3>Follow Us on Social Media</h3>
+                <p>Welcome to our online gallery showcasing the rich artistic heritage of Ethiopia. We are dedicated to promoting Ethiopian artists, photographers, and sculptors, providing a  platform for their remarkable work to be admired and cherished.
+
+Our goal is to celebrate the diverse cultural expressions and creativity of Ethiopian talent. We curate a collection of artworks that encapsulate the essence of Ethiopian artistry, offering a glimpse into the beauty of Ethiopian art.
+
+Through our platform, we connect art enthusiasts, collectors, and admirers with the vibrant and captivating creations of Ethiopian artists. We strive to foster a deeper appreciation for the cultural richness and artistic brilliance that Ethiopia has to offer.
+
+Explore our gallery to discover a captivating array of paintings, photographs, and sculptures.</p>
+                <h5>Follow Us on Social Media</h5>
                 <a href="#" className="btn btn-primary mx-2"><FontAwesomeIcon icon={faFacebook} /></a>
                 <a href="#" className="btn btn-primary mx-2"><FontAwesomeIcon icon={faInstagram} /></a>
                 <a href="#" className="btn btn-primary mx-2"><FontAwesomeIcon icon={faTwitter} /></a>
